20d0cac874a3218f53e2aaf653172e15330c2957
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2023-06-19 23:30:26 +0530

    port changes

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 
-axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
+axios.defaults.baseURL = "https://cric-frick-server.onrender.com";
 
 
 /** Make API Requests */
